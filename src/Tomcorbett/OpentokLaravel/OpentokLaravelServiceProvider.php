4f544030477683532e6b212d6c6daf6856eb720b
Salva Camarasa <salva.camarasa@visyon360.com>
2016-07-19 13:46:20 +0200

    Tagging of config file publishing

--- src/Tomcorbett/OpentokLaravel/OpentokLaravelServiceProvider.php
+++ src/Tomcorbett/OpentokLaravel/OpentokLaravelServiceProvider.php
@@ -22,7 +22,7 @@ class OpentokLaravelServiceProvider extends ServiceProvider {
 	{
 	    $this->publishes([
 	        __DIR__.'/config/config.php' => config_path('opentok-laravel.php')
-	    ]);
+	    ], 'config');
 	}
 
 	/**
